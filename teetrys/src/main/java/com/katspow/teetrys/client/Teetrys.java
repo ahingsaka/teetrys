5b7dafb6b54ff1e6f9f85d5610656f3c608d2a2f
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2013-10-10 11:09:21 +0200

    add main class

---
+++ teetrys/src/main/java/com/katspow/teetrys/client/Teetrys.java
@@ -0,0 +1,34 @@
+package com.katspow.teetrys.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.katspow.caatja.core.Caatja;
+import com.katspow.caatjagwt.client.CAATGwt;
+import com.katspow.caatjagwt.client.CaatjaGwtDate;
+import com.katspow.caatjagwt.client.CaatjaGwtEventManager;
+import com.katspow.caatjagwt.client.CaatjaGwtImageLoader;
+import com.katspow.caatjagwt.client.CaatjaGwtNavigator;
+import com.katspow.caatjagwt.client.CaatjaGwtPreloader;
+import com.katspow.caatjagwt.client.CaatjaGwtRootPanel;
+import com.katspow.caatjagwt.client.CaatjaGwtService;
+import com.katspow.caatjagwt.client.CaatjaGwtWindow;
+import com.katspow.teetrys.client.core.GameController;
+
+public class Teetrys implements EntryPoint {
+
+    public void onModuleLoad() {
+        
+        new Caatja(new CaatjaGwtDate(), new CaatjaGwtNavigator(), new CaatjaGwtWindow(), new CaatjaGwtRootPanel(),
+                new CaatjaGwtService(), new CaatjaGwtEventManager(), new CaatjaGwtImageLoader(),
+                new CaatjaGwtPreloader(), new CAATGwt());
+        
+        try {
+//            new GameController().start();
+        } catch (Exception e) {
+            Window.alert("Could not load teetrys !");
+            e.printStackTrace();
+        }
+        
+    }
+
+}
