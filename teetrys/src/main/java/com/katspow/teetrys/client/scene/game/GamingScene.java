6b2b1e74f24761ce700981bfe990e21bdfdcfa82
AHINGSAKA SAMSENESENA <samizaka@gmail.com>
2013-10-10 11:22:16 +0200

    add scenes : gaming, main menu

---
+++ teetrys/src/main/java/com/katspow/teetrys/client/scene/game/GamingScene.java
@@ -0,0 +1,7 @@
+package com.katspow.teetrys.client.scene.game;
+
+import com.katspow.caatja.foundation.Scene;
+
+public class GamingScene extends Scene {
+
+}
---
+++ teetrys/src/main/java/com/katspow/teetrys/client/scene/menus/MainMenuScene.java
@@ -0,0 +1,7 @@
+package com.katspow.teetrys.client.scene.menus;
+
+import com.katspow.caatja.foundation.Scene;
+
+public class MainMenuScene extends Scene {
+
+}
